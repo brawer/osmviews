89d02df9e12c810e6fa50e907a9580db3cf37552
Sascha Brawer <sascha@brawer.ch>
2022-05-07 16:35:22 +0200

    Compute command to restart webserver upon new release

--- prod/deploy_release.py
+++ prod/deploy_release.py
@@ -17,7 +17,12 @@ import shutil
 import urllib.request
 
 
-def deploy_release(tag=None):
+def deploy_release(tag='latest'):
+    path = download_release(tag)
+    restart_webserver(path)
+
+
+def download_release(tag):
     url = 'https://api.github.com/repos/brawer/osmviews/releases/'
     if tag is None or tag is 'latest':
         url += 'latest'
@@ -25,17 +30,29 @@ def deploy_release(tag=None):
         url += 'tags/' + tag
     with urllib.request.urlopen(url) as response:
         info = json.loads(response.read())
-    tag = info['tag_name']  # latest -> actual tag
+    tag = info['tag_name']  # 'latest' -> actual tag
+    assets = info['assets']
     print('Deploying release', tag)
-    path = pathlib.PurePath('bin', tag)
-    shutil.rmtree(path, ignore_errors=True)
-    pathlib.Path(path).mkdir(parents=True)
-    for asset in info['assets']:
+    path = pathlib.Path('bin', tag)
+    tmp_path = pathlib.Path('bin', 'tmp-%s.tmp' % tag)
+    shutil.rmtree(tmp_path, ignore_errors=True)
+    tmp_path.mkdir(parents=True)
+    for asset in assets:
         with urllib.request.urlopen(asset['browser_download_url']) as response:
-            filepath = pathlib.PurePath(path, asset['name'])
+            filepath = pathlib.PurePath(tmp_path, asset['name'])
             with open(filepath, 'wb') as f:
                 shutil.copyfileobj(response, f)
             os.chmod(filepath, 0o0755)
+    shutil.rmtree(path, ignore_errors=True)
+    tmp_path.rename(path)
+    return path
+
+
+def restart_webserver(path):
+    binary = (path / 'webserver').resolve()
+    cmd = ['webserver', '--backend=kubernetes', 'golang',
+           'restart', str(binary)]
+    print(' '.join(cmd))
 
 
 if __name__ == '__main__':
